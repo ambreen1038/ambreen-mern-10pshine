37b0231fa4793b571d36a848824c7f8ded5e8dde
Ambreen <ambreenhabib1038@gmail.com>
2025-08-11 12:52:30 +0500

    feature/frontend/dashboard

--- frontend/src/services/download.js
+++
@@ -1,9 +0,0 @@
-export function downloadTextFile(filename, text) {
-  const element = document.createElement("a");
-  const file = new Blob([text], { type: "text/plain" });
-  element.href = URL.createObjectURL(file);
-  element.download = filename;
-  document.body.appendChild(element);
-  element.click();
-  document.body.removeChild(element);
-}
